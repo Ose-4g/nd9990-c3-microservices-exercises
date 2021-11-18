2d54f56dc4f881b7d196d918d98415ec2bfdea0a
OSEMUDIAMEN ITUA <osemudiamenitua@gmail.com>
2021-11-18 13:31:30 +0100

    fix: fix non connection to postgresql kubernetes

--- project/udagram-api-user/src/sequelize.ts
+++ project/udagram-api-user/src/sequelize.ts
@@ -3,8 +3,8 @@ import {config} from './config/config';
 
 
 export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
+  'username': String(config.username).trim(),
+  'password': String(config.password).trim(),
   'database': config.database,
   'host': config.host,
 
